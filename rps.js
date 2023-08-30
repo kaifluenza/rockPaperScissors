6a8fb89099351459c854f8d0ece7e03fdd4cb17d
kai34000 <wanrueni.12@gmail.com>
2023-08-30 13:04:34 -0400

    Add UI and CSS

--- rps.js
+++ rps.js
@@ -1,14 +1,3 @@
-//examine the document object
-// console.log(document.URL);
-// document.title = "RPS";
-// console.log(document.title);
-// console.log(document.body);
-//console.log(document.images);
-
-
-//selectors: get element by id
-
-
 function getComputerChoice() {
     let ranNum = Math.floor(Math.random()*3); //expected:0,1,or 2
     if (ranNum===0) return "rock";
@@ -33,38 +22,54 @@ function playRound(playerSelection, computerSelection){
            "tie";
    }
    else{
-    return "Your input is invalid. Input rock, paper, or scissors!";
+    return "Invalid!";
    }
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection,computerSelection));
-
-
-
-function game(){
-    let playerSelection = "";
-    let computerSelection="";
-    let playerWinCount = 0;
-    let playerLossCount = 0;
-    for(let i=0; i<5; i++){ 
-        //should loop until playerWinCount==5
-        playerSelection = prompt("Enter your choice: rock, paper, or scirssors:");
-        computerSelection = getComputerChoice();
-        result = playRound(playerSelection,computerSelection);
-        if(result==="win") { 
-            alert("You won!");
-            playerWinCount++;
-        }
-        else if(result==="loss") {
-            console.log("You lost!");
-            playerLossCount++;
-        }
-        else if(result==="tie"){
-            console.log("It's a tie!");
-        }
+function clickHandler(choice){
+    //on click, play a round with player chosen r/p/s
+    currentResult=playRound(choice,getComputerChoice());
+    //update score based on result 
+    if(currentResult==="win"){
+        playerWinCount++;
+    }
+    else if(currentResult==="loss"){
+        computerWinCount++;
     }
 
-    return "You won " + playerWinCount + " times and lost " + playerLossCount + " times.";
+    if(playerWinCount==5){
+        alert("You won!");
+        resetGame();
+    }else if(computerWinCount==5){
+        alert("You lost!");
+        resetGame();
+    }
+    //display score after each round to the div score
+    playerScore.textContent = playerWinCount;
+    computerScore.textContent = computerWinCount;
+
+
 }
+
+function resetGame(){
+    playerWinCount=0;
+    computerWinCount=0;
+    playerScore.textContent = playerWinCount;
+    computerScore.textContent = computerWinCount;
+}
+
+let playerWinCount = 0;
+let computerWinCount = 0;
+
+//buttons
+let btnR = document.querySelector('#r');
+let btnP = document.querySelector('#p');
+let btnS = document.querySelector('#s');
+//divs
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
+//event handlers
+btnR.addEventListener('click',()=>{clickHandler("rock")});
+btnP.addEventListener('click',()=>{clickHandler("paper")});
+btnS.addEventListener('click', ()=>{clickHandler("scissors")});
+
